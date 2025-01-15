357198c1b752d3ae034daa1d7126199c568d601e
th-kairi <kairi.fujii@mail.o-hara.ac.jp>
2025-01-15 09:48:52 +0900

    mod template

--- testSite001/main/admin.py
+++ testSite001/main/admin.py
@@ -9,8 +9,14 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ['is_password_encrypted']
     search_fields = ['email', 'name']
 
+class MemberAdmin(UserAdmin):
+    model = Member
+    list_display = ['member_num', 'name', 'email', 'is_password_encrypted']
+    list_filter = ['is_password_encrypted']
+    search_fields = ['email', 'name']
+
 # Register your models here.
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(position)
 admin.site.register(Employee)
-admin.site.register(Member)
+admin.site.register(Member, MemberAdmin)
