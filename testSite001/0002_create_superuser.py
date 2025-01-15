bcd510d9c6b1fbf5e8f39eb51ab0f05efeeae580
th-kairi <kairi.fujii@mail.o-hara.ac.jp>
2025-01-15 10:08:28 +0900

    mod admin.site

--- testSite001/0002_create_superuser.py
+++ testSite001/0002_create_superuser.py
@@ -1,15 +1,37 @@
 from django.db import migrations
 from django.contrib.auth.models import User
-from main.models import CustomUser  # CustomUser モデルをインポート
+from main.models import *
 
 def create_superuser(apps, schema_editor):
     # スーパーユーザーの情報を設定
-    CustomUser.objects.create_superuser(
+    user = CustomUser.objects.create_superuser(
         username='admin',
+        name='administraotr',
         email='admin@mail.com',
         password='admin',  # セキュアなパスワードを使用する
         is_password_encrypted=True,
     )
+    user.is_staff = True  # スタッフ権限も付与
+    user.is_superuser = True  # スーパーユーザーとして設定
+    user.save()
+
+    employee = Employee.objects.create_user(
+        username='employee',
+        name='employee',
+        email='employee@mail.com',
+        password='employee',  # セキュアなパスワードを使用する
+        is_password_encrypted=True,
+    )
+    employee.save()
+
+    member = Member.objects.create_user(
+        username='member',
+        name='member',
+        email='member@mail.com',
+        password='member',  # セキュアなパスワードを使用する
+        is_password_encrypted=True,
+    )
+    member.save()
 
 class Migration(migrations.Migration):
 
--- testSite001/accounts/admin.py
+++ testSite001/accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+# import * とすることでmodels.pyで定義した全てのモデルをインポートする
+from main.models import *
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['name', 'email', 'is_password_encrypted']
+    list_filter = ['is_password_encrypted']
+    search_fields = ['email', 'name']
+
+class MemberAdmin(admin.ModelAdmin):
+    model = Member
+    list_display = ['name', 'email', 'is_password_encrypted']
+    list_filter = ['is_password_encrypted']
+    search_fields = ['email', 'name']
+    
+class EmployeeAdmin(admin.ModelAdmin):
+    model = Employee
+    list_display = ['name', 'email', 'is_password_encrypted']
+    list_filter = ['is_password_encrypted','division', 'team', 'position']
+    search_fields = ['email', 'name', 'division', 'team', 'position']
+
+# Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(position)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Member, MemberAdmin)
